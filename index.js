abe2613ec45ef0e128de35f372ab57ff3bebcbd6
usnar <usna.rafiqzadah@gmail.com>
2022-10-05 14:55:09 -0400

    MVP Completed

--- index.js
+++ index.js
@@ -15,10 +15,22 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+ this.name = name;
+ this.age = age;
+ this.stomach = [];
 }
-
+Person.prototype.eat = function(edible) {
+ if(this.stomach.length<10){
+  this.stomach.push(edible);
+ }
+}
+Person.prototype.poop = function(){
+this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
 /*
   TASK 2
@@ -36,11 +48,26 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon; 
+this.tank = 0;
+this.odometer = 0;
 }
-
-
+Car.prototype.fill = function(gallons){
+this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance){
+  const drivableMiles = this.tank * this.milesPerGallon;
+  if(distance <= drivableMiles){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance / this.milesPerGallon);
+  } else {
+    this.odometer = this.odometer + drivableMiles;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles`;
+  }
+}
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -49,18 +76,21 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+ Person.call(this, name, age);
+ this.favoriteToy = favoriteToy;
 }
-
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window Binding - When none of the other rules apply, "this" will return the window or global object in node or undefined in strict mode
+  2. Implicit Binding - When the function is invoked, look to the left of the dot that what "this" refers to. 
+  3. Explicit Binding - When you pass in .call(pass arguments one by one, immediately invokes function), .apply(pass arguments as array, immediately invokes), or .bind(pass arguments one by one and does not immediately invoke, returns new function to be invoked later) then "this" is explicitly defined. 
+  4. New Binding - when a function is created as a constructor "this" points to the newly created object. 
 */
 
 ///////// END OF CHALLENGE /////////
